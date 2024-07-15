597d713aa016a755ca03bfed80e73d3ec6168a1d
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-15 18:31:56 +0100

    Add documentation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+"""
+A type-anotated function add that takes a float a and a float b as arguments
+
+Arguments:
+    a: float
+    b: float
+Returns:
+    The sum of a and b as a float.
+"""
 def add(a: float, b: float) -> float:
     return a + b
