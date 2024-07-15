41543327a9e33d4e727f9a655a93e83d91c0b66e
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-15 18:50:25 +0100

    Returns a concated string using a type annotated function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function 'concat' that takes
+a string str1 and a string str2 as arguments
+
+Arguments:
+    str1: a string
+    str2: a string
+
+Returns:
+    a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns a concatenated string of str1 and str2
+    """
+    return str1 + str2
