9e5681d2c093c94131ad4f1d0a5233e9c14cab01
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-16 00:52:36 +0100

    A function that takes in a float and returns a function that multiplies a float by a float

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function make_multiplier that
+takes a float multiplier as argument and returns
+a function that multiplies a float by multiplier.
+"""
+
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that multiplies a float by multiplier.
+    """
+    def multiply(n: float) -> float:
+        """
+        Returns the product of n and multiplier.
+        """
+        return n * multiplier
+    return multiply
