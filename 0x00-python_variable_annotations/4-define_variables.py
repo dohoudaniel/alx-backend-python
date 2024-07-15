59f810a9648741803204f31d9ac5d0cc4c13f8ae
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-15 19:29:57 +0100

    Define and annotating variables with value

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Defining and annotating variables
+with a specific value
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
