34bb3b52a3c6b393d574535e083086419519a8df
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-15 19:09:59 +0100

    Returns the floor of a float passed in as an argument

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+A type-anotated function 'floor' that
+takes a float n as argument
+and returns the floor of the float.
+"""
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float
+    """
+    return int(n)
