42791916642c66d49c5d752fb5fb3ab1b9802dfc
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-16 22:01:34 +0100

    Returns the total execution time of a coroutine

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+A function that takes in two integers as
+arguments and measures the total execution
+time for wait_n(n, max_delay), and returns
+the total time / n.
+This function should return a float
+"""
+
+
+# Import statements
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    Returns the total execution time
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    execution_time = end_time - start_time
+    return (execution_time / n)
