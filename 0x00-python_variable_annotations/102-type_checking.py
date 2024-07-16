2038eabaa2b12d760763858b9b06cd015cacbb8b
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-16 09:54:02 +0100

    Import Tuple, and change passed argument to a tuple

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -6,10 +6,10 @@ an intger and returns a tuple of integers
 
 
 # Import statement
-from typing import List
+from typing import List, Tuple
 
 
-def zoom_array(lst: List[int], factor: int = 2) -> List:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Returns a tuple of integers
     """
@@ -20,7 +20,7 @@ def zoom_array(lst: List[int], factor: int = 2) -> List:
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
