b34d868781d2b07adc5da33d3cb82f84c5970e2a
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-16 22:22:44 +0100

    Rename function

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -10,7 +10,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_await_random(max_delay: int) -> asyncio.Task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Returns an asyncio.task
     """
