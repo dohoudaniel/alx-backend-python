d7cdf7e87535661a33b8136f0b37119753991861
dohoudaniel <dohoudanielfavour@gmail.com>
2024-07-16 09:19:16 +0100

    Fix Pycodestyle errors

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -11,9 +11,9 @@ from typing import Mapping, Any, Union, TypeVar
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, \
-                    default: Union[T, None] \
-                        = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None]
+                     = None) -> Union[Any, T]:
     """
     Returns the value of a key in a
     dictionary if it exists,
