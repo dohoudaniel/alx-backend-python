d5fe0db5a1e42537bbdc9b69ddf3457827d948cf
dohoudaniel <dohoudanielfavour@gmail.com>
2025-09-21 23:49:32 +0100

    feat: store url in a variable

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -86,6 +86,7 @@ class TestGithubOrgClient(unittest.TestCase):
         - assert public_repos returns expected list of names
         - assert mocked property and get_json were called once
         """
+        ghUrl = "https://api.github.com/orgs/google/repos"
         # Prepare a fake repos payload.
         repos_payload: List[Dict[str, Any]] = [
             {"name": "repo1", "license": {"key": "mit"}},
@@ -99,7 +100,7 @@ class TestGithubOrgClient(unittest.TestCase):
         with patch.object(client.GithubOrgClient,
                           "_public_repos_url",
                           new_callable=PropertyMock) as mock_pub_url:
-            mock_pub_url.return_value = "https://api.github.com/orgs/google/repos"
+            mock_pub_url.return_value = ghUrl
 
             github_client = client.GithubOrgClient("google")
 
